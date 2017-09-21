8da52797ee4806f45c62f2f92997b5a3432c6620
AlexVyi <alexandru@epk.ro>
2017-09-22 00:18:23 +0300

    added all side directive and menu.must make it responsive

--- controllers/directives.js
+++ controllers/directives.js
@@ -9,5 +9,62 @@ var app =  angular.module('slidesDirective',['ngRoute'])
 			.when('/myth', {
 			templateUrl: 'slides/myth.html'
 
-		});
-	}])
+			})
+
+			.when('/degrade', {
+				templateUrl: 'slides/degrade.html'
+
+			})
+			.when('/single', {
+				templateUrl: 'slides/singleusemyth.html'
+
+			})
+			.when('/recycling', {
+				templateUrl: 'slides/recycling.html'
+
+			})
+			.when('/pvspbag', {
+				templateUrl: 'slides/papervsplastic.html'
+
+			})
+			.when('/pvspstudies', {
+				templateUrl: 'slides/papervsplasticstudies.html'
+
+			})
+			.when('/reuse', {
+				templateUrl: 'slides/reusablegreener.html'
+
+			})
+			.when('/types', {
+				templateUrl: 'slides/typesofbags.html'
+
+			})
+			.when('/litter', {
+				templateUrl: 'slides/litterthefacts.html'
+
+			})
+			.when('/phealth', {
+				templateUrl: 'slides/publichealth.html'
+
+			})
+			.when('/ca', {
+				templateUrl: 'slides/canadaupdate.html'
+
+			})
+			.when('/bags', {
+				templateUrl: 'slides/bagsarroundtheworld.html'
+
+			})
+			.when('/oilmyth', {
+				templateUrl: 'slides/oilmyth.html'
+
+			})
+			.when('/made', {
+				templateUrl: 'slides/madeincanada.html'
+
+			})
+			.when('/ire', {
+				templateUrl: 'slides/irelandbagtax.html'
+
+			})
+	}]);
