10e6f20e0833d9ec54edb964cb085eb9ec012d38
AlexVyi <alexandru@epk.ro>
2017-09-26 16:35:56 +0300

    main III

--- controllers/pretty.js
+++ controllers/pretty.js
@@ -1,10 +1,10 @@
 
     function toggleMenu () {
-    var check = document.getElementById('show-menu');
-    var checkLbl = document.getElementById('checkLabel')
-    if(check.checked){
-        checkLbl.innerHTML = "Arata meniul"
-    }else {
+        var check = document.getElementById('show-menu');
+        var checkLbl = document.getElementById('checkLabel')
+      if(check.checked){
+         checkLbl.innerHTML = "Arata meniul"
+      }else {
         checkLbl.innerHTML = "Ascunde meniul"
     }
 }
