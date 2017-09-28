0a4323d49df2ef13b2c8505a0f4d2f70d9a48d42
AlexVyi <alexandru@epk.ro>
2017-09-29 01:03:35 +0300

    finished myth.added anchor for scroll and finished main.started bans

--- controllers/app.js
+++ controllers/app.js
@@ -1,1 +1,13 @@
-var app =  angular.module('pungi',['ui.bootstrap','slidesDirective']);
+var app =  angular.module('pungi',['ui.bootstrap','slidesDirective'])
+
+	.controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+		function($scope, $location, $anchorScroll) {
+			$scope.gotoTop = function() {
+				// set the location.hash to the id of
+				// the element you wish to scroll to.
+				$location.hash('top');
+
+				// call $anchorScroll()
+				$anchorScroll();
+			};
+		}]);
